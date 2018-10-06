aff0dbdef8ec1c1a451b7717646a9dc82fba3224
Xavier Trinidad <xmtrinidad@gmail.com>
2018-10-06 15:56:13 -0700

    Added remaining cards, added card section responsiveness

--- src/app/overview-section/overview-section.component.ts
+++ src/app/overview-section/overview-section.component.ts
@@ -17,7 +17,12 @@ export class OverviewSectionComponent implements OnInit {
     {cardNum: 63, cardTitle: 'Doctors', icon: 'home'},
     {cardNum: 340, cardTitle: 'Nurse', icon: 'accessibility'},
     {cardNum: 460, cardTitle: 'Patient', icon: 'favorite'},
-    {cardNum: 60, cardTitle: 'Pharmacist', icon: 'face'}
+    {cardNum: 60, cardTitle: 'Pharmacist', icon: 'face'},
+    {cardNum: 30, cardTitle: 'Lab', icon: 'business'},
+    {cardNum: 40, cardTitle: 'Accountants', icon: 'money'},
+    {cardNum: 2601, cardTitle: 'Operation', icon: 'people'},
+    {cardNum: 2601, cardTitle: 'Birth Reports', icon: 'note'},
+    {cardNum: 2601, cardTitle: 'Death Reports', icon: 'warning'},
   ];
 
   constructor() { }
